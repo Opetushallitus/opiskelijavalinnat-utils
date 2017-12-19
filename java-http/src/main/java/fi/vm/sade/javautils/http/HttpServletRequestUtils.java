95b87f722c0cab77ec22c3f27139173687bfb38d
Timo Rantalaiho <timo.rantalaiho@reaktor.com>
2017-12-19 10:57:42 +0200

    Allow skipping logging.
    E.g. koodisto adds audit logging data to all requests,
    including buildversion.txt requests done by internal
    load balancers. Those requests do not have these headers
    and it is normal.

--- java-http/src/main/java/fi/vm/sade/javautils/http/HttpServletRequestUtils.java
+++ java-http/src/main/java/fi/vm/sade/javautils/http/HttpServletRequestUtils.java
@@ -1,13 +1,28 @@
 package fi.vm.sade.javautils.http;
 
+import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.function.Predicate;
 
 public class HttpServletRequestUtils {
     private static final Logger LOGGER = LoggerFactory.getLogger(HttpServletRequestUtils.class);
+    private static final Set<String> HARMLESS_URLS = parseHarmlessUrlsFromSystemProperty();
+
+    private static Set<String> parseHarmlessUrlsFromSystemProperty() {
+        String property = System.getProperty("fi.vm.sade.javautils.http.HttpServletRequestUtils.HARMLESS_URLS");
+        if (StringUtils.isBlank(property)) {
+            return Collections.emptySet();
+        }
+        return new HashSet<>(Arrays.asList(property.split(",")));
+    }
+
     public static String getRemoteAddress(HttpServletRequest httpServletRequest) {
         String xRealIp = httpServletRequest.getHeader("X-Real-IP");
         Predicate<String> isNotBlank = (String txt) -> txt != null && !txt.isEmpty();
@@ -22,7 +37,10 @@ public class HttpServletRequestUtils {
             }
             return xForwardedFor;
         }
-        LOGGER.warn("X-Real-IP or X-Forwarded-For was not set. Are we not running behind a load balancer?");
+        String requestURI = httpServletRequest.getRequestURI();
+        if (!HARMLESS_URLS.contains(requestURI)) {
+            LOGGER.warn(String.format("X-Real-IP or X-Forwarded-For was not set. Are we not running behind a load balancer? Request URI is '%s'", requestURI));
+        }
         return httpServletRequest.getRemoteAddr();
     }
 }
