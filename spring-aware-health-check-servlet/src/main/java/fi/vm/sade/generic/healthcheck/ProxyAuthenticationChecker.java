Opetushallitus/opiskelijavalinnat-utils

commit 5b0dd840450eb51be72386b2e74623831f3fc2e4

OK-233 : Caller-Id instead of clientSubsystemCode
Trying to make it mandatory in more places too.

spring-aware-health-check-servlet/pom.xml
@@ -75,7 +75,7 @@
       <dependency>
         <groupId>fi.vm.sade.java-utils</groupId>
         <artifactId>legacy-caching-rest-client</artifactId>
-        <version>0.1.0-SNAPSHOT</version>
+        <version>0.2.0-SNAPSHOT</version>
       </dependency>
       <dependency>
         <groupId>fi.vm.sade.java-utils</groupId>

spring-aware-health-check-servlet/src/main/java/fi/vm/sade/generic/healthcheck/ProxyAuthenticationChecker.java
@@ -26,7 +26,7 @@ public class ProxyAuthenticationChecker implements HealthChecker {
         String currentUser = auth.getName();
 
         String currentAppId = servletContext.getContextPath().replaceAll("/", "");
-        CachingRestClient restClient = new CachingRestClient().setClientSubSystemCode(currentAppId + ".ProxyAuthenticationChecker");
+        CachingRestClient restClient = new CachingRestClient(currentAppId + ".ProxyAuthenticationChecker");
         restClient.setUseProxyAuthentication(true);
         restClient.setWebCasUrl(getProperty("web.url.cas"));
         String appurl = getProperty("cas.service." + currentAppId);

httpclient/pom.xml
@@ -9,7 +9,7 @@
     </parent>
 
     <artifactId>httpclient</artifactId>
-    <version>0.2.1-SNAPSHOT</version>
+    <version>0.3.0-SNAPSHOT</version>
     <packaging>jar</packaging>
 
     <dependencies>

java-auth/pom.xml
@@ -10,7 +10,7 @@
         <relativePath>..</relativePath>
     </parent>
     <artifactId>java-auth</artifactId>
-    <version>0.1.1-SNAPSHOT</version>
+    <version>0.2.0-SNAPSHOT</version>
     <packaging>jar</packaging>
     <dependencies>
         <dependency>
@@ -21,7 +21,7 @@
         <dependency>
             <groupId>fi.vm.sade.java-utils</groupId>
             <artifactId>httpclient</artifactId>
-            <version>0.2.1-SNAPSHOT</version>
+            <version>0.3.0-SNAPSHOT</version>
         </dependency>
     </dependencies>
 </project>

java-cas/pom.xml
@@ -8,7 +8,7 @@
     <relativePath>..</relativePath>
   </parent>
   <artifactId>java-cas</artifactId>
-  <version>0.3.1-SNAPSHOT</version>
+  <version>0.4.0-SNAPSHOT</version>
   <packaging>jar</packaging>
   <dependencies>
     <dependency>
@@ -19,7 +19,7 @@
     <dependency>
       <groupId>fi.vm.sade.java-utils</groupId>
       <artifactId>httpclient</artifactId>
-      <version>0.2.1-SNAPSHOT</version>
+      <version>0.3.0-SNAPSHOT</version>
     </dependency>
     <dependency>
       <groupId>commons-lang</groupId>

java-cxf/pom.xml
@@ -12,7 +12,7 @@
 
     <groupId>fi.vm.sade.java-utils</groupId>
     <artifactId>java-cxf</artifactId>
-    <version>0.2.1-SNAPSHOT</version>
+    <version>0.3.0-SNAPSHOT</version>
     <!-- exclusions due <dependencyConvergence/> -->
     <dependencies>
         <dependency>

java-http/pom.xml
@@ -10,7 +10,7 @@
     <modelVersion>4.0.0</modelVersion>
 
     <artifactId>java-http</artifactId>
-    <version>0.1.6-SNAPSHOT</version>
+    <version>0.2.0-SNAPSHOT</version>
 
     <properties>
         <apache.httpclient.version>4.5.2</apache.httpclient.version>
@@ -21,7 +21,7 @@
         <dependency>
             <groupId>fi.vm.sade.java-utils</groupId>
             <artifactId>java-cas</artifactId>
-            <version>0.3.1-SNAPSHOT</version>
+            <version>0.4.0-SNAPSHOT</version>
         </dependency>
 
         <dependency>

java-ldap/pom.xml
@@ -37,7 +37,7 @@
     <dependency>
       <groupId>fi.vm.sade.java-utils</groupId>
       <artifactId>opintopolku-spring-security</artifactId>
-      <version>0.1.0-SNAPSHOT</version>
+      <version>0.2.0-SNAPSHOT</version>
     </dependency>
     <dependency>
       <groupId>org.springframework.security</groupId>

commit ab2ae23c7c4fc4dfde661f45a82545d5cc2165db

OK-233 : Restore old version of dependency
spring-aware-health-check-servlet version needs to be increased too
when upgrading its dependencies.

This fixes the issue introduced in commit 5b0dd840450eb51be72386b2e74623831f3fc2e4.

spring-aware-health-check-servlet/pom.xml
@@ -75,7 +75,7 @@
       <dependency>
         <groupId>fi.vm.sade.java-utils</groupId>
         <artifactId>legacy-caching-rest-client</artifactId>
-        <version>0.2.0-SNAPSHOT</version>
+        <version>0.1.0-SNAPSHOT</version>
       </dependency>
       <dependency>
         <groupId>fi.vm.sade.java-utils</groupId>

spring-aware-health-check-servlet/src/main/java/fi/vm/sade/generic/healthcheck/ProxyAuthenticationChecker.java
@@ -26,7 +26,7 @@ public class ProxyAuthenticationChecker implements HealthChecker {
         String currentUser = auth.getName();
 
         String currentAppId = servletContext.getContextPath().replaceAll("/", "");
-        CachingRestClient restClient = new CachingRestClient(currentAppId + ".ProxyAuthenticationChecker");
+        CachingRestClient restClient = new CachingRestClient().setClientSubSystemCode(currentAppId + ".ProxyAuthenticationChecker");
         restClient.setUseProxyAuthentication(true);
         restClient.setWebCasUrl(getProperty("web.url.cas"));
         String appurl = getProperty("cas.service." + currentAppId);