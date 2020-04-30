64dc1c6f5f3138772a821c009bccfdbd782408e9
Timo Rantalaiho <timo.rantalaiho@reaktor.com>
2020-04-30 09:33:27 +0300

    Lisää client-ip-tieto myös lokaalisti
    X-Real-Ip- ja X-Forwarded-For-otsakkeet ovat vain
    palvelinympäristöissä käytössä.

--- src/clj_ring_db_session/session/session_client.clj
+++ src/clj_ring_db_session/session/session_client.clj
@@ -5,7 +5,8 @@
   (fn [{:keys [headers] :as req}]
       (let [user-agent (:user-agent headers)
             client-ip  (or (get headers "x-real-ip")
-                           (get headers "x-forwarded-for"))]
+                           (get headers "x-forwarded-for")
+                           (:remote-addr req))]
         (handler (-> req
                      (assoc-in [:session :user-agent] user-agent)
                      (assoc-in [:session :client-ip] client-ip))))))
