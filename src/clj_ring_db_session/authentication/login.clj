dc0637cc274cb0b111931c425de1394cd5356faf
Timo Rantalaiho <timo.rantalaiho@reaktor.com>
2020-05-08 16:02:57 +0300

    Yksinkertaista login-koodia

--- src/clj_ring_db_session/authentication/login.clj
+++ src/clj_ring_db_session/authentication/login.clj
@@ -2,9 +2,13 @@
   (:require [clj-ring-db-session.authentication.cas-ticketstore :as cas-store]
             [ring.util.response :as resp]))
 
-(defn- do-login [virkailija-finder henkilo-finder do-on-success success-redirect-url username ticket]
-  (let [virkailija (virkailija-finder username)
-        henkilo    (henkilo-finder (:oidHenkilo virkailija))]
+(defn login [params]
+  (let [{:keys [username
+                henkilo
+                ticket
+                success-redirect-url
+                datasource]} params]
+    (cas-store/login ticket datasource)
     (-> (resp/redirect success-redirect-url)
         (assoc :session {:identity {:username   username
                                     :first-name (:kutsumanimi henkilo)
@@ -13,29 +17,7 @@
                                     :lang       (or (some #{(-> henkilo :asiointiKieli :kieliKoodi)}
                                                           ["fi" "sv" "en"])
                                                     "fi")
-                                    :ticket     ticket}})
-        (do-on-success virkailija henkilo username ticket))))
-
-(defn login
-  "login-provider function returns username and valid service ticket when login was successful.
-   virkailija-finder resolves virkailija user by username.
-   henkilo-finder resolves personal data by person oid."
-  [params]
-  (let [{:keys [login-provider
-                virkailija-finder
-                henkilo-finder
-                success-redirect-url
-                do-on-success
-                login-failed-handler
-                datasource]} params]
-    (try
-      (if-let [[username ticket] (login-provider)]
-        (do
-          (cas-store/login ticket datasource)
-          (do-login virkailija-finder henkilo-finder do-on-success success-redirect-url username ticket))
-        (login-failed-handler))
-      (catch Exception e
-        (login-failed-handler e)))))
+                                    :ticket     ticket}}))))
 
 (defn logout [session logout-url datasource]
   (cas-store/logout (-> session :identity :ticket) datasource)
