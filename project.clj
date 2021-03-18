bb863e50979c38b682329a02d2c6324e4e11d529
Tatu Honkanen <tatu.honkanen@cybercom.com>
2021-03-18 15:25:24 +0200

    OY-2817 : Kirjastot päivitetty

--- project.clj
+++ project.clj
@@ -1,18 +1,18 @@
-(defproject oph/clj-ring-db-cas-session "0.3.0-SNAPSHOT"
+(defproject oph/clj-ring-db-cas-session "0.3.1-SNAPSHOT"
   :description "Clojure-kirjasto Opintoplun CAS-kirjautumiseen ja session tallentamiseen tietokantaan."
   :url "http://example.com/FIXME"
   :license {:name "EUPL, Version 1.1 or - as soon as they will be approved by the European Commission - subsequent versions of the EUPL (the \"Licence\")"
             :url  "http://www.osor.eu/eupl/"}
 
-  :dependencies [[org.clojure/clojure "1.10.1"]
-                 [ring "1.8.0"]
-                 [metosin/ring-http-response "0.9.1"]
+  :dependencies [[org.clojure/clojure "1.10.3"]
+                 [ring "1.9.1"]
+                 [metosin/ring-http-response "0.9.2"]
                  [metosin/compojure-api "1.1.13"]
                  [buddy/buddy-auth "2.2.0"]
                  [yesql "0.5.3"]]
 
   :plugins [[lein-modules "0.3.11"]
-            [lein-ancient "0.6.15"]
+            [lein-ancient "0.7.0"]
             [lein-less "1.7.5"]
             [lein-shell "0.5.0"]]
 
