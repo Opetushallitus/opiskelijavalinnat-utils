ce59b56a4f784baa5bc09078ab5a128da11ecf29
Tatu Honkanen <tatu.honkanen@cybercom.com>
2021-04-29 12:09:29 +0300

    OY-2817 : validateServiceTicketWithVirkailijaUsername added

--- java-cas/src/main/java/fi/vm/sade/javautils/nio/cas/CasClient.java
+++ java-cas/src/main/java/fi/vm/sade/javautils/nio/cas/CasClient.java
@@ -2,8 +2,18 @@ package fi.vm.sade.javautils.nio.cas;
 
 import io.netty.handler.codec.http.cookie.Cookie;
 import io.netty.handler.codec.http.cookie.DefaultCookie;
-import org.asynchttpclient.*;
+import org.asynchttpclient.AsyncHttpClient;
+import org.asynchttpclient.Request;
+import org.asynchttpclient.RequestBuilder;
+import org.asynchttpclient.Response;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.w3c.dom.Document;
+import org.xml.sax.InputSource;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.StringReader;
 import java.util.Date;
 import java.util.Set;
 import java.util.concurrent.CompletableFuture;
@@ -13,7 +23,7 @@ import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.Function;
 
 import static fi.vm.sade.javautils.nio.cas.CasSessionFetchProcess.emptySessionProcess;
-import static org.asynchttpclient.Dsl.*;
+import static org.asynchttpclient.Dsl.asyncHttpClient;
 
 /*
  Usage example:
@@ -32,131 +42,165 @@ import static org.asynchttpclient.Dsl.*;
     casClient.execute(req).thenApply(response -> System.out.println(response.getStatusCode()));
  */
 public class CasClient {
-  private final CasConfig config;
-  private final AsyncHttpClient asyncHttpClient;
-  private final AtomicReference<CasSessionFetchProcess> sessionStore =
-    new AtomicReference<>(emptySessionProcess());
-  private final long estimatedValidToken = TimeUnit.MINUTES.toMillis(15);
+    private static final Logger logger = LoggerFactory.getLogger(CasClient.class);
+    private final CasConfig config;
+    private final AsyncHttpClient asyncHttpClient;
+    private final AtomicReference<CasSessionFetchProcess> sessionStore =
+            new AtomicReference<>(emptySessionProcess());
+    private final long estimatedValidToken = TimeUnit.MINUTES.toMillis(15);
 
-  public CasClient(CasConfig config) {
-    this.config = config;
-    this.asyncHttpClient = asyncHttpClient();
-  }
-
-  private String tgtLocationFromResponse(Response casResponse) {
-    if (201 == casResponse.getStatusCode()) {
-      return casResponse.getHeader("Location");
-    } else {
-      throw new RuntimeException("Couldn't get TGT ticket!");
+    public CasClient(CasConfig config) {
+        this.config = config;
+        this.asyncHttpClient = asyncHttpClient();
     }
-  }
 
-  private String ticketFromResponse(Response casResponse) {
-    if (200 == casResponse.getStatusCode()) {
-      return casResponse.getResponseBody().trim();
-    } else {
-      throw new RuntimeException("Couldn't get session ticket from CAS!");
-    }
-  }
-
-  private CasSession newSessionFromToken(String token) {
-    return new CasSession(token, new Date(System.currentTimeMillis() + estimatedValidToken));
-  }
-
-  private CasSession sessionFromResponse(Response casResponse) {
-    for (Cookie cookie : casResponse.getCookies()) {
-      if (config.getjSessionName().equals(cookie.name())) {
-        CasSession session = newSessionFromToken(cookie.value());
-        return session;
-      }
-    }
-    throw new RuntimeException(String.format("%s cookie not in CAS authentication response!", config.getjSessionName()));
-  }
-
-  private Request withCsrfAndCallerId(Request req) {
-    return req.toBuilder()
-      .setHeader("Caller-Id", config.getCallerId())
-      .addOrReplaceCookie(new DefaultCookie("CSRF", config.getCsrf()))
-      .build();
-  }
-
-  public CasSessionFetchProcess sessionRequest(CasSessionFetchProcess currentSession) {
-    Request tgtReq = withCsrfAndCallerId(new RequestBuilder()
-      .setUrl(String.format("%s/v1/tickets", config.getCasUrl()))
-      .setMethod("POST")
-      .addFormParam("username", config.getUsername())
-      .addFormParam("password", config.getPassword())
-      .build());
-    final String serviceUrl = String.format("%s%s",
-      config.getServiceUrl(),
-      config.getServiceUrlSuffix()
-    );
-    CompletableFuture<CasSession> responsePromise = asyncHttpClient.executeRequest(tgtReq)
-      .toCompletableFuture().thenCompose(response -> {
-
-        Request req = withCsrfAndCallerId(new RequestBuilder()
-          .setUrl(tgtLocationFromResponse(response))
-          .setMethod("POST")
-          .addFormParam("service", serviceUrl)
-          .build());
-        return asyncHttpClient.executeRequest(req).toCompletableFuture();
-      }).thenCompose(response -> {
-
-        Request req = withCsrfAndCallerId(new RequestBuilder()
-          .setUrl(serviceUrl)
-          .setMethod("GET")
-          .addQueryParam("ticket", ticketFromResponse(response))
-          .build());
-        return asyncHttpClient.executeRequest(req).toCompletableFuture();
-      }).thenApply(this::sessionFromResponse);
-    final CasSessionFetchProcess newFetchProcess = new CasSessionFetchProcess(responsePromise);
-
-    if (sessionStore.compareAndSet(currentSession, newFetchProcess)) {
-      return newFetchProcess;
-    } else {
-      responsePromise.cancel(true);
-      return sessionStore.get();
-    }
-  }
-
-  private Function<Response, Response> onSuccessIncreaseSessionTime(CasSessionFetchProcess currentSessionProcess, CasSession session) {
-    return response -> {
-      if (Set.of(200, 201).contains(response.getStatusCode())) {
-        if (sessionStore.compareAndSet(currentSessionProcess, new CasSessionFetchProcess(
-          CompletableFuture.completedFuture(
-            newSessionFromToken(session.getSessionCookie()))))) {
-          //System.out.println("Updated current session with more time");
+    private String tgtLocationFromResponse(Response casResponse) {
+        if (201 == casResponse.getStatusCode()) {
+            return casResponse.getHeader("Location");
         } else {
-          //System.out.println("Tried to update more time to current session but some other thread was faster");
+            throw new RuntimeException("Couldn't get TGT ticket!");
         }
-      }
-      return response;
-    };
-  }
-
-  private CompletableFuture<Response> executeRequestWithSession(CasSession session, Request request) {
-    return asyncHttpClient.executeRequest(withCsrfAndCallerId(request.toBuilder()
-      .addOrReplaceCookie(new DefaultCookie(config.getjSessionName(), session.getSessionCookie()))
-      .build())).toCompletableFuture();
-  }
-
-  private CompletableFuture<Response> executeRequestWithReusedSession(CasSessionFetchProcess currentSessionProcess, CasSession session, Request request) {
-    return executeRequestWithSession(session, request).thenApply(onSuccessIncreaseSessionTime(currentSessionProcess, session));
-  }
-
-  public CompletableFuture<Response> execute(Request request) {
-    final CasSessionFetchProcess currentSession = sessionStore.get();
-    return currentSession.getSessionProcess()
-      .thenCompose(session ->
-        session.isValid() ? executeRequestWithReusedSession(currentSession, session, request) :
-          sessionRequest(currentSession).getSessionProcess().thenCompose(newSession -> executeRequestWithSession(newSession, request)));
-  }
-
-  public Response executeBlocking(Request request) throws ExecutionException {
-    try {
-      return execute(request).get();
-    } catch (Exception e) {
-      throw new ExecutionException("Failed to execute blocking request: " + request.getUrl(), e);
     }
-  }
+
+    private String ticketFromResponse(Response casResponse) {
+        if (200 == casResponse.getStatusCode()) {
+            return casResponse.getResponseBody().trim();
+        } else {
+            throw new RuntimeException("Couldn't get session ticket from CAS!");
+        }
+    }
+
+    private CasSession newSessionFromToken(String token) {
+        return new CasSession(token, new Date(System.currentTimeMillis() + estimatedValidToken));
+    }
+
+    private CasSession sessionFromResponse(Response casResponse) {
+        for (Cookie cookie : casResponse.getCookies()) {
+            if (config.getjSessionName().equals(cookie.name())) {
+                CasSession session = newSessionFromToken(cookie.value());
+                return session;
+            }
+        }
+        throw new RuntimeException(String.format("%s cookie not in CAS authentication response!", config.getjSessionName()));
+    }
+
+    private Request withCsrfAndCallerId(Request req) {
+        return req.toBuilder()
+                .setHeader("Caller-Id", config.getCallerId())
+                .addOrReplaceCookie(new DefaultCookie("CSRF", config.getCsrf()))
+                .build();
+    }
+
+    public CasSessionFetchProcess sessionRequest(CasSessionFetchProcess currentSession) {
+        Request tgtReq = withCsrfAndCallerId(new RequestBuilder()
+                .setUrl(String.format("%s/v1/tickets", config.getCasUrl()))
+                .setMethod("POST")
+                .addFormParam("username", config.getUsername())
+                .addFormParam("password", config.getPassword())
+                .build());
+        final String serviceUrl = String.format("%s%s",
+                config.getServiceUrl(),
+                config.getServiceUrlSuffix()
+        );
+        CompletableFuture<CasSession> responsePromise = asyncHttpClient.executeRequest(tgtReq)
+                .toCompletableFuture().thenCompose(response -> {
+
+                    Request req = withCsrfAndCallerId(new RequestBuilder()
+                            .setUrl(tgtLocationFromResponse(response))
+                            .setMethod("POST")
+                            .addFormParam("service", serviceUrl)
+                            .build());
+                    return asyncHttpClient.executeRequest(req).toCompletableFuture();
+                }).thenCompose(response -> {
+
+                    Request req = withCsrfAndCallerId(new RequestBuilder()
+                            .setUrl(serviceUrl)
+                            .setMethod("GET")
+                            .addQueryParam("ticket", ticketFromResponse(response))
+                            .build());
+                    return asyncHttpClient.executeRequest(req).toCompletableFuture();
+                }).thenApply(this::sessionFromResponse);
+        final CasSessionFetchProcess newFetchProcess = new CasSessionFetchProcess(responsePromise);
+
+        if (sessionStore.compareAndSet(currentSession, newFetchProcess)) {
+            return newFetchProcess;
+        } else {
+            responsePromise.cancel(true);
+            return sessionStore.get();
+        }
+    }
+
+    private Function<Response, Response> onSuccessIncreaseSessionTime(CasSessionFetchProcess currentSessionProcess, CasSession session) {
+        return response -> {
+            if (Set.of(200, 201).contains(response.getStatusCode())) {
+                if (sessionStore.compareAndSet(currentSessionProcess, new CasSessionFetchProcess(
+                        CompletableFuture.completedFuture(
+                                newSessionFromToken(session.getSessionCookie()))))) {
+                    //System.out.println("Updated current session with more time");
+                } else {
+                    //System.out.println("Tried to update more time to current session but some other thread was faster");
+                }
+            }
+            return response;
+        };
+    }
+
+    private CompletableFuture<Response> executeRequestWithSession(CasSession session, Request request) {
+        return asyncHttpClient.executeRequest(withCsrfAndCallerId(request.toBuilder()
+                .addOrReplaceCookie(new DefaultCookie(config.getjSessionName(), session.getSessionCookie()))
+                .build())).toCompletableFuture();
+    }
+
+    private CompletableFuture<Response> executeRequestWithReusedSession(CasSessionFetchProcess currentSessionProcess, CasSession session, Request request) {
+        return executeRequestWithSession(session, request).thenApply(onSuccessIncreaseSessionTime(currentSessionProcess, session));
+    }
+
+    public CompletableFuture<Response> execute(Request request) {
+        final CasSessionFetchProcess currentSession = sessionStore.get();
+        return currentSession.getSessionProcess()
+                .thenCompose(session ->
+                        session.isValid() ? executeRequestWithReusedSession(currentSession, session, request) :
+                                sessionRequest(currentSession).getSessionProcess().thenCompose(newSession -> executeRequestWithSession(newSession, request)));
+    }
+
+    public Response executeBlocking(Request request) throws ExecutionException {
+        try {
+            return execute(request).get();
+        } catch (Exception e) {
+            throw new ExecutionException(String.format("Failed to execute blocking request: %s", request.getUrl()), e);
+        }
+    }
+
+    public CompletableFuture<String> validateServiceTicketWithVirkailijaUsername(String service, String ticket) {
+
+        logger.info("VALIDATING TICKET: " + ticket + " , service: " + service);
+
+        Request req = withCsrfAndCallerId(new RequestBuilder()
+                .setUrl(config.getCasUrl() + "/serviceValidate?ticket=" + ticket + "&service=" + service)
+                .addQueryParam("ticket", ticket)
+                .addQueryParam("service", service)
+                .setMethod("GET")
+                .build());
+        return asyncHttpClient.executeRequest(req).toCompletableFuture().thenApply(this::getUsernameFromResponse);
+    }
+
+    public String validateServiceTicketWithVirkailijaUsernameBlocking(String service, String ticket) throws ExecutionException {
+      try {
+        return validateServiceTicketWithVirkailijaUsername(service, ticket).get();
+      } catch (Exception e) {
+        throw new ExecutionException(String.format("Failed to validate service ticket with virkailija username, service: %s , ticket: &s", service, ticket), e);
+      }
+    }
+
+    private String getUsernameFromResponse(Response response) {
+        try {
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            Document document = builder.parse(new InputSource(new StringReader(response.getResponseBody())));
+            logger.info("Validated!");
+            return document.getElementsByTagName("cas:user").item(0).getTextContent();
+        } catch (Exception e) {
+            throw new RuntimeException("CAS service ticket validation failed: ", e);
+        }
+    }
 }
