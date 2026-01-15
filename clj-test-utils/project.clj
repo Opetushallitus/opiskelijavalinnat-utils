7b4849d444e25960829c9d00fdf3fe785a13ab09
August Kilponen <august.kilponen@gmail.com>
2026-01-15 14:21:25 +0200

    Päivitetty testcontainers

--- clj-test-utils/project.clj
+++ clj-test-utils/project.clj
@@ -1,16 +1,16 @@
-(defproject opiskelijavalinnat-utils/clj-test-utils "0.5.6-SNAPSHOT"
+(defproject opiskelijavalinnat-utils/clj-test-utils "0.5.7-SNAPSHOT"
             :description "oph clojure testing utilities"
             :url "http://example.com/FIXME"
             :license {:name "EUPL"
                       :url "http://www.osor.eu/eupl/"}
             :plugins [[lein-modules "0.3.11"]]
-            :dependencies [[oph/clj-s3 "0.2.5-SNAPSHOT"]
-                           [oph/clj-log "0.3.1-SNAPSHOT"]
+            :dependencies [[opiskelijavalinnat-utils/clj-s3 "0.2.5-SNAPSHOT"]
+                           [opiskelijavalinnat-utils/clj-log "0.3.1-SNAPSHOT"]
                            [com.amazonaws/aws-java-sdk-s3 "1.11.978"]
                            [io.findify/s3mock_2.12 "0.2.6"]
                            [base64-clj "0.1.1"]
-                           [org.testcontainers/testcontainers "1.17.6"]
-                           [org.testcontainers/elasticsearch "1.17.6"]
+                           [org.testcontainers/testcontainers "2.0.1"]
+                           [org.testcontainers/elasticsearch "2.0.1"]
                            [robert/hooke "1.3.0"]]
             :deploy-repositories [["snapshots" {:url           "https://maven.pkg.github.com/Opetushallitus/opiskelijavalinnat-utils"
                                                 :username      "private-token"
